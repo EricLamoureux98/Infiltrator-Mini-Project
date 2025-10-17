2581c3d5419d4c4a5dafe51d559d290440e3ca15
Eric Lamoureux <ericandbailey@gmail.com>
2025-10-17 10:53:20 -0400

    Ninja Animations
    Walk and Idle using animation blend tree

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    Rigidbody2D rb;
+    Animator anim;
+    Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * speed;
+    }
+    
+    public void Move(InputAction.CallbackContext context)
+    {
+        anim.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetFloat("LastInputX", moveInput.x);
+            anim.SetFloat("LastInputY", moveInput.y);
+        }
+
+        moveInput = context.ReadValue<Vector2>();
+        anim.SetFloat("InputX", moveInput.x);
+        anim.SetFloat("InputY", moveInput.y);
+    }
+}
